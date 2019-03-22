b11b1b392b5c7e6833d10311d1dc1bb49ba34a70
Johan Walhout <johanwalhout@gmail.com>
2019-03-22 09:43:59 +0100

    Prepare

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,45 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
+import 'package:flutter_compass/flutter_compass.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double _direction;
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterCompass.events.listen((double direction) {
+      setState(() {
+        _direction = direction;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Compass'),
+        ),
         body: Container(
-          child: Text('Compass'),
+          alignment: Alignment.center,
+          color: Colors.white,
+          child: Transform.rotate(
+            angle: ((_direction ?? 0) * (math.pi / 180) * -1),
+            child: Image.asset('assets/compass.jpg'),
+          ),
         ),
       ),
     );
   }
 }
-
 
