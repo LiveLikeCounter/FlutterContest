6dde55b45094098201666fc53102717be3a8c6a0
kowsik paduchuri <p.kowsik5@gmail.com>
2019-03-22 21:21:22 -0400

    Added logic for placing a location

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,26 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   double _direction;
+//  double _lat1 = 36.778259;
+//  double lat2 = 27.950575;
+//  ouble _long1 = -119.417931;
+//  double long2 = -82.457176;
+
+  double dy = 28.0834343 - 28.0827146;
+  double dx = math.cos(math.pi/180*(28.0827146))*((82.4138412) - (82.4112165));
+  double angle;
 
   @override
   void initState() {
     super.initState();
+
+
+
     FlutterCompass.events.listen((double direction) {
       setState(() {
         _direction = direction;
+        angle = math.atan2(dy, dx);
+        print(_direction);
       });
     });
   }
@@ -34,8 +47,16 @@ class _MyAppState extends State<MyApp> {
           alignment: Alignment.center,
           color: Colors.white,
           child: Transform.rotate(
-            angle: ((_direction ?? 0) * (math.pi / 180) * -1),
-            child: Image.asset('assets/compass.jpg'),
+            angle: ((_direction ?? 0) * (math.pi/180) * -1),
+            child: Stack(
+              children: <Widget>[
+              Image.asset('assets/compass.jpg'),
+            Transform.rotate(
+              angle: ((angle ?? 0)),
+              child: Image.asset('assets/compass1.png'),
+            ),
+            ],
+            ),
           ),
         ),
       ),
